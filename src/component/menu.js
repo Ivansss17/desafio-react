6bcd76ef230124348b7429ba7b85894bc32632a1
ivan <ivansss17@gmail.com>
2021-12-18 20:21:56 -0300

    menu-react-1

--- src/component/menu.js
+++ src/component/menu.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Menu() {
     return (
         <div className='divMenu'>
+            <h2>Bienvenidos a la tienda virtual</h2>
             <nav className='navMenu'>
                 <ul>
                     <li>Home</li>
